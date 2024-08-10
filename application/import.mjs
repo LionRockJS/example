f9e0edfad02b6df1c9523d312ca6d6e1af9d5da6
Colin Leung <colin@cowise.co>
2024-08-11 00:50:33 +0800

    update session mixin and adapter

--- application/import.mjs
+++ application/import.mjs
@@ -1,17 +1,21 @@
 import { Central, ControllerMixinDatabase, Model } from '@lionrockjs/central';
 import { View } from '@lionrockjs/mvc';
 import AdapterViewLiquid, { LiquidView } from '@lionrockjs/adapter-view-liquidjs';
-View.DefaultViewClass = LiquidView;
-
 import { ORMAdapterSQLite, DatabaseAdapterBetterSQLite3 } from "@lionrockjs/adapter-database-better-sqlite3";
+import MixinSession, {ControllerMixinSession} from '@lionrockjs/mixin-session';
+import AdapterSessionDatabase, { SessionDatabase } from "@lionrockjs/adapter-session-database";
+
+View.DefaultViewClass = LiquidView;
 Model.defaultAdapter = ORMAdapterSQLite;
 ControllerMixinDatabase.defaultAdapter = DatabaseAdapterBetterSQLite3;
+ControllerMixinSession.defaultAdapter = SessionDatabase;
 
 await (async () => {
   Central.addModules([
     AdapterViewLiquid,
+    MixinSession,
+    AdapterSessionDatabase,
     await import('@lionrockjs/mixin-form'),
-    await import('@lionrockjs/mixin-session'),
     await import('@lionrockjs/mod-auth'),
     await import('@lionrockjs/adapter-auth-password'),
     await import('@lionrockjs/mod-admin'),
