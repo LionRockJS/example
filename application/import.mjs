ed29bca91fa9dfe301aa8a4cedd280eea01d0525
Colin Leung <colin@cowise.co>
2024-08-16 02:11:22 +0800

    update session to jwt

--- application/import.mjs
+++ application/import.mjs
@@ -1,5 +1,4 @@
-import { Central, ControllerMixinDatabase, Model } from '@lionrockjs/central';
-import { View } from '@lionrockjs/mvc';
+import { Central, ControllerMixinDatabase, Model, View } from '@lionrockjs/central';
 import AdapterViewLiquid, { LiquidView } from '@lionrockjs/adapter-view-liquidjs';
 View.DefaultViewClass = LiquidView;
 
@@ -7,11 +6,15 @@ import { ORMAdapterSQLite, DatabaseAdapterBetterSQLite3 } from "@lionrockjs/adap
 Model.defaultAdapter = ORMAdapterSQLite;
 ControllerMixinDatabase.defaultAdapter = DatabaseAdapterBetterSQLite3;
 
+import MixinSection, {ControllerMixinSession} from '@lionrockjs/mixin-session';
+import {SessionJWT} from '@lionrockjs/adapter-session-jwt';
+ControllerMixinSession.defaultAdapter = SessionJWT;
+
 await (async () => {
   Central.addModules([
     AdapterViewLiquid,
+    MixinSection,
     await import('@lionrockjs/mixin-form'),
-    await import('@lionrockjs/mixin-session'),
     await import('@lionrockjs/mod-auth'),
     await import('@lionrockjs/adapter-auth-password'),
     await import('@lionrockjs/mod-admin'),
