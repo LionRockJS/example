d0119e2b16534a1a870efb6fe9e4048d43748d73
Colin Leung <colin@cowise.co>
2024-08-16 02:17:18 +0800

    fix: user database

--- application/config/auth.mjs
+++ application/config/auth.mjs
@@ -4,6 +4,9 @@ import {IdentifierPassword} from '@lionrockjs/adapter-auth-password';
 
 export default {
   databasePath: path.normalize(Central.EXE_PATH + '/../database'),
+  databaseMap: new Map([
+    ['admin', path.normalize(Central.EXE_PATH + '/../database/admin.sqlite')],
+  ]),
   userDatabase: 'admin.sqlite',
   databaseMapName : "admin",
   identifiers: [IdentifierPassword],
