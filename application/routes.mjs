dc39bc4477b07e6e06e50f5e69ac5cbc91cf525e
Colin Leung <colin@cowise.co>
2024-10-09 23:50:44 +0800

    chore: update router

--- application/routes.mjs
+++ application/routes.mjs
@@ -1,6 +1,8 @@
-import { RouteList, Central } from '@lionrockjs/central';
-import path from "path";
-import fs from "fs";
+import { Central } from '@lionrockjs/central';
+import { RouteList } from '@lionrockjs/router';
+
+import path from "node:path";
+import fs from "node:fs";
 
 RouteList.add('/', 'controller/Home');
 RouteList.add('/pages/:slug', 'controller/Home', 'page');
