933fb4b9b2932b2c9b1a73cfd5125b79b0eca93d
Colin Leung <colin@cowise.co>
2024-10-14 00:29:08 +0800

    chore: simplify import.mjs

--- application/import.mjs
+++ application/import.mjs
@@ -1,20 +1,18 @@
 import { Central } from '@lionrockjs/central';
 
-await (async () => {
-  Central.addModules([
-    await import('@lionrockjs/mixin-form'),
-    await import('@lionrockjs/mixin-session'),
+await Central.addModules([
+  await import('@lionrockjs/mixin-form'),
+  await import('@lionrockjs/mixin-session'),
 
-    await import('@lionrockjs/adapter-view-liquidjs'),
-    await import('@lionrockjs/adapter-database-better-sqlite3'),
-    await import('@lionrockjs/adapter-auth-password'),
-    await import('@lionrockjs/adapter-mail-aws'),
-    await import('@lionrockjs/adapter-session-jwt'),
+  await import('@lionrockjs/adapter-view-liquidjs'),
+  await import('@lionrockjs/adapter-database-better-sqlite3'),
+  await import('@lionrockjs/adapter-auth-password'),
+  await import('@lionrockjs/adapter-mail-aws'),
+  await import('@lionrockjs/adapter-session-jwt'),
 
-    await import('@lionrockjs/mod-auth'),
-    await import('@lionrockjs/mod-admin'),
-    await import('@lionrockjs/mod-mail'),
-    await import('@lionrockjs/mod-cold-leads'),
-    await import('@lionrockjs/mod-admin-rsvp'),
-  ]);
-})();
+  await import('@lionrockjs/mod-auth'),
+  await import('@lionrockjs/mod-admin'),
+  await import('@lionrockjs/mod-mail'),
+  await import('@lionrockjs/mod-cold-leads'),
+  await import('@lionrockjs/mod-admin-rsvp'),
+]);
