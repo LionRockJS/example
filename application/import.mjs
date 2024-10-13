9ac809558d33e072905ff38dc4aee1986b47bcb4
Colin Leung <colin@cowise.co>
2024-10-14 01:12:20 +0800

    chore: v2 import

--- application/import.mjs
+++ application/import.mjs
@@ -1,24 +1,12 @@
-import { Central, ControllerMixinDatabase, Model } from '@lionrockjs/central';
-import { View } from '@lionrockjs/mvc';
-import AdapterViewLiquid, { LiquidView } from '@lionrockjs/adapter-view-liquidjs';
-View.DefaultViewClass = LiquidView;
+import { Central } from '@lionrockjs/central';
 
-import { ORMAdapterSQLite, DatabaseAdapterBetterSQLite3 } from "@lionrockjs/adapter-database-better-sqlite3";
-Model.defaultAdapter = ORMAdapterSQLite;
-ControllerMixinDatabase.defaultAdapter = DatabaseAdapterBetterSQLite3;
-
-import MixinSession, {ControllerMixinSession} from '@lionrockjs/mixin-session';
-import {SessionJWT} from '@lionrockjs/adapter-session-jwt';
-ControllerMixinSession.defaultAdapter = SessionJWT;
-import AdapterPassword from '@lionrockjs/adapter-auth-password';
-
-await (async () => {
-  Central.addModules([
-    AdapterViewLiquid,
-    await import('@lionrockjs/mixin-form'),
-    MixinSession,
-    await import('@lionrockjs/mod-auth'),
-    AdapterPassword,
-    await import('@lionrockjs/mod-admin'),
-  ]);
-})();
+await Central.addModules([
+  await import("@lionrockjs/adapter-database-better-sqlite3"),
+  await import('@lionrockjs/adapter-view-liquidjs'),
+  await import('@lionrockjs/mixin-form'),
+  await import('@lionrockjs/mixin-session'),
+  await import('@lionrockjs/mod-auth'),
+  await import('@lionrockjs/adapter-auth-password'),
+  await import('@lionrockjs/mod-admin'),
+  await import('@lionrockjs/adapter-session-jwt'),
+]);
