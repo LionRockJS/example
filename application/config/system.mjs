d854e7a18f933ac1d63dd6e1d796cc7b018dee95
Colin Leung <colin@cowise.co>
2024-10-17 18:08:41 +0800

    update style

--- application/config/system.mjs
+++ application/config/system.mjs
@@ -1,6 +1,6 @@
 import {Central} from '@lionrockjs/central';
 
 export default {
-  debug: (Central.ENV !== Central.ENV_PROD),
-  serve_static_file: Central.ENV === Central.ENV_DEVE,
+  debug: true,//(Central.ENV !== Central.ENV_PROD),
+  serve_static_file: true//Central.ENV === Central.ENV_DEVE,
 }
