30f0d834897695dd08bd363b66eacdfda23716dc
Colin Leung <colin@cowise.co>
2025-05-15 00:40:43 +0800

    ncu

--- application/config/system.mjs
+++ application/config/system.mjs
@@ -1,6 +1,6 @@
 import {Central} from '@lionrockjs/central';
 
 export default {
-  debug: (Central.ENV !== Central.ENV_PROD),
+  debug: (Central.ENV !== Central.ENV_PRODUCTION),
   serve_static_file: false,
 }
