708170ef8e0a257841a02228062f55befe89c7cb
Colin Leung <colin@cowise.co>
2025-12-19 02:13:08 +0800

    Update system.mjs

--- application/config/system.mjs
+++ application/config/system.mjs
@@ -1,6 +1,10 @@
-import {Central} from '@lionrockjs/central';
+import {Central, CentralEnv} from '@lionrockjs/central';
+import { ServerAdapterNodeHTTP } from "@lionrockjs/platform-web-node-http";
 
 export default {
-  debug: (Central.ENV !== Central.ENV_PROD),
-  serve_static_file: false,
+  debug: (Central.ENV !== CentralEnv.PRODUCTION),
+  serve_static_file: Central.ENV === CentralEnv.DEVELOPMENT,
+  platform:{
+    adapter: ServerAdapterNodeHTTP
+  }
 }
