0097e3ec6321939e5f3f87d69dc0035c64af7fd9
Colin Leung <colin@cowise.co>
2025-12-21 03:27:26 +0800

    Upgrade LionrockJS dependencies to v2 and switch to bun-sqlite
    Updated all @lionrockjs dependencies to version 2.0.0 and replaced @lionrockjs/adapter-database-better-sqlite3 with @lionrockjs/adapter-database-bun-sqlite. Also commented out the import of @lionrockjs/view-admin in import.mjs.

--- application/import.mjs
+++ application/import.mjs
@@ -5,12 +5,12 @@ await Central.addModules([
   await import('@lionrockjs/mixin-session'),
 
   await import('@lionrockjs/adapter-view-liquidjs'),
-  await import('@lionrockjs/adapter-database-better-sqlite3'),
+  await import('@lionrockjs/adapter-database-bun-sqlite'),
   await import('@lionrockjs/adapter-auth-password'),
   await import('@lionrockjs/adapter-session-jwt'),
 
   await import('@lionrockjs/mod-auth'),
   await import('@lionrockjs/mod-admin'),
 
-  await import('@lionrockjs/view-admin'),
+//  await import('@lionrockjs/view-admin'),
 ]);
