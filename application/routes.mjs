01294f898532682254eabdf8651f9bdbd8103c63
Colin Leung <colin@cowise.co>
2026-01-08 01:52:42 +0800

    Use Central.modules instead of Central.nodePackages
    Replaces usage of Central.nodePackages with Central.modules when loading module routes. This ensures the correct set of modules is iterated for dynamic route imports.

--- application/routes.mjs
+++ application/routes.mjs
@@ -6,7 +6,7 @@ import fs from "node:fs";
 RouteList.add('/', 'controller/Home');
 RouteList.add('/pages/:slug', 'controller/Home', 'page');
 
-await Promise.all([...Central.nodePackages.values()].map( async x => {
+await Promise.all([...Central.modules.values()].map( async x => {
   const filePath = path.normalize(`${x}/routes.mjs`);
   if (!fs.existsSync(filePath)) return;
   await import('file://' + filePath);
