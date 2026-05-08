2b7b7f9e7ef7f1bc44071540a45845a3bb67c1a2
Colin Leung <colin@cowise.co>
2026-05-08 16:21:57 +0800

    chore: remove site config from bootstrap

--- application/bootstrap.mjs
+++ application/bootstrap.mjs
@@ -2,7 +2,6 @@ import { Central } from '@lionrockjs/central';
 import {HelperTranslate} from "@lionrockjs/adapter-view-liquidjs";
 
 await Central.initConfig(new Map([
-  ['site', await import('./config/site.mjs')],
   ['liquidjs', await import('./config/liquidjs.mjs')],
 ]));
 
