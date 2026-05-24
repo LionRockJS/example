baf3a6b4a7e2d17ab439be1e00bb08937998e0eb
Colin Leung <colin@cowise.co>
2026-05-25 02:44:31 +0800

    Update Server.ts

--- server/Server.ts
+++ server/Server.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { getCookie, setCookie } from 'hono/cookie';
 import { RouteList } from '@lionrockjs/router';
 import {Central} from '@lionrockjs/central';
 
@@ -19,16 +20,26 @@ const routes = Array.from(RouteList.routeMap.values());
 routes.forEach((route: any) => {
   app.on(route.method, route.path, async c => {
     console.log(route.controller);
-    const Controller = (await import(`../application/classes/${route.controller}.ts`)).default;
+    let Controller;
+    try {
+      Controller = (await import(`../application/classes/${route.controller}.ts`)).default;
+    } catch (e) {
+      Controller = Central.resolveController(route.controller);
+      if (!Controller) throw e;
+    }
     const controller = new Controller(
       {...c.req, 
         params: c.req.param(),
+        query: c.req.query(),
         headers: c.req.header(),
+        cookies: getCookie(c),
         env: c.env,
       }
     );
     const result = await controller.execute(route.action);
-    return c.html(result.body, result.status);
+    Object.entries(result.headers).forEach(([key, value]) => c.header(key, value));
+    result.cookies.forEach(cookie => setCookie(c, cookie.name, cookie.value, cookie.options));
+    return c.html(result.body, result.status as any);
   });
 });
 
